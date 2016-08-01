4c8a16e899e4895a98a7514460f89c089978d64e
Prashant Farkya <prashant.farkya@gmail.com>
2016-08-01 14:03:34 +0530

    dataAdd object create

--- js/crudNgApp.js
+++ js/crudNgApp.js
@@ -12,6 +12,7 @@ angular.module('crudNgApp', ['ngRoute'])
     .controller('mainController', ['$scope', '$http', function($scope, $http) {
         $scope.hello = "hello";
         $scope.keys = ["id", "name"];
+        $scope.dataAdd = {};
         return $http({
             method: 'GET',
             url: 'http://localhost:8080/records?_start=0&_limit=1'
@@ -19,6 +20,9 @@ angular.module('crudNgApp', ['ngRoute'])
                 // $log.log("hahahhhahha respond" + JSON.stringify(response));
                 if (response.data[0]) {
                     $scope.keys = Object.keys(response.data[0]);
+                    $scope.keys.forEach(function(key) {
+                        if (key !== 'id') $scope.dataAdd[key] = "";
+                    });
                 } else {
                     alert("not record found");
                 }
