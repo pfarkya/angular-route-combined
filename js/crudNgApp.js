70bf833786e0cba60cdc6d7f7c2d2a9e8c0dc4e7
unknown <vishal221092@gmail.com>
2016-08-01 18:04:45 +0530

    w

--- js/crudNgApp.js
+++ js/crudNgApp.js
@@ -1,22 +1,66 @@
 angular.module('crudNgApp', ['ngRoute'])
-    .config(function($routeProvider, $locationProvider) {
+    .config(function($routeProvider) {
         $routeProvider
             .when('/list', {
                 templateUrl: 'listitem.html',
                 controller: 'ListController',
             })
-            // configure html5 to get links working on jsfiddle
-        $locationProvider.html5Mode(true);
+            .when('/add',{
+                templateUrl:'additem.html',
+                controller: 'AddController',
+            })
     })
-    .controller('mainController', ['$scope', function($scope) {
+    .controller('mainController', ['$scope', '$http', function($scope, $http) {
         $scope.hello = "hello";
+        $scope.keys = ["id", "name"];
+        $scope.dataAdd = {};
+        return $http({
+            method: 'GET',
+            url: 'http://localhost:8081/records?_start=0&_limit=1'
+        }).then(function successCallback(response) {
+                // $log.log("hahahhhahha respond" + JSON.stringify(response));
+                if (response.data[0]) {
+                    $scope.keys = Object.keys(response.data[0]);
+                    $scope.keys.forEach(function(key) {
+                        if (key != 'id') 
+                            $scope.dataAdd[key] = "";
+                    });
+                } else {
+                    alert("not record found");
+                }
+            },
+            function errorCallback(response) {
+                alert("not get" + response.message);
+            });
     }])
-    .controller('ListController', ['$scope', function($scope) {
+    .controller('ListController', ['$scope', '$http', function($scope, $http) {
         $scope.hello = "welcome";
         $scope.records = [];
-        $http('', url, post, function(status, response) {
-            // success
-        }, function(status, response) {
-            // error
-        });
+        return $http({
+            method: 'GET',
+            url: 'http://localhost:8081/records?_start=0&_limit=20'
+        }).then(function successCallback(response) {
+                // $log.log("hahahhhahha respond" + JSON.stringify(response));
+                if (response.data[0]) {
+                    $scope.records = response.data;
+                } else {
+                    alert("not record found");
+                }
+            },
+            function errorCallback(response) {
+                alert("not get" + response.message);
+            });
+    }])
+    .controller('AddController',['$scope','$http',function($scope,$http){
+        $scope.addItem=function(){
+                alert("qw");
+               return $http({
+                method: 'POST',
+                url: 'http://localhost:8081/records',
+                data: $scope.dataAdd,
+                'content-type': "application/json",
+            }).then(function successCallback(response){
+                $scope.showMessage="Successfully Added";
+            });
+        }
     }])
